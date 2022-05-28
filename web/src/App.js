492b1449d19e61d6219513ee9423be371b0e8644
hermes926 <hermes926@gmail.com>
2022-05-28 16:18:23 +0800

    Simple Header

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from "web3/dist/web3.min";
+
+function getLibrary(provider) {
+  return new Web3(provider)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <Header />
+      </div>
+    </Web3ReactProvider>
   );
 }
+
+
 
 export default App;
---
+++ web/src/header/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import ConnectButton from '../component/ConnectButton';
+import './index.css';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar sticky="top" expand="lg">
+                <Container className="mx-3 my-1" fluid>
+                    <Navbar.Brand className="header-brand" href="/" style={{ marginLeft: '1%'}}>
+                        NFT Lending!!!
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Item style={{ marginLeft: '1%' }}>
+                                <Nav.Link href="/lend" style={{ color: 'rgb(100, 100, 100)' }}>
+                                    Lend
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item style={{ marginLeft: '1%' }}>
+                                <Nav.Link href="/borrow" style={{ color: 'rgb(100, 100, 100)' }}>
+                                    Borrow
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <Nav>
+                            <Nav.Item>
+                                <ConnectButton />
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default Header;
